959d2ec1391c306fedeb80dc375a4c57970ec629
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-21 17:27:46 -0400

    Clase 3

--- PrimerEjemplo/src/Pizza.java
+++ PrimerEjemplo/src/Pizza.java
@@ -22,6 +22,44 @@ public class Pizza {
         this.precio = precio;
     }
     // 3.- getter and setter (accesador y mutador)
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getMasa() {
+        return masa;
+    }
+
+    public void setMasa(String masa) {
+        this.masa = masa;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
     
     // 4.- Métodos personalizados (customer)
+    
+    public void preparar()
+    {
+        // sout + tab
+        System.out.println("La pizza se está preparando...");
+    }
 }
