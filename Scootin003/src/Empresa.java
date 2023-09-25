ddb93fc793fc35af53ac06d7f15f774a4d415e99
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-25 18:46:37 -0300

    unidad 2

---
+++ Scootin003/src/Empresa.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Empresa {
+    //ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+    private ArrayList<Empleado> empleados;
+    
+    public Empresa()
+    {
+        empleados = new ArrayList<Empleado>();
+    }
+    
+    public boolean agregar(Empleado empleado)
+    {
+        // verificar que no se repita el rut
+        return empleados.add(empleado);
+    }
+    public boolean buscarEmpleado(String rut)
+    {
+        for(Empleado empleado: empleados)
+        {
+            if(empleado.getRut().equals(rut))
+                return true;
+        }
+        return false;
+    }
+    public void listarEmpleado()
+    {
+        for(Empleado empleado: empleados)
+        {
+            System.out.println(empleado.imprimir());
+        }
+    }
+    public boolean eliminarEmpleado(String rut)
+    {
+        for(Empleado empleado: empleados)
+        {
+            if(empleado.getRut().equals(rut))
+                return empleados.remove(empleado);
+        }
+        return false;
+    }
+}
