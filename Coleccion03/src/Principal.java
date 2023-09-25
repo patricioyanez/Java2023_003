c2486540d8f0cf01f57950f2a21ff453b8a7844f
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-25 17:32:15 -0300

    unidad 2

--- Coleccion03/src/Principal.java
+++ Coleccion03/src/Principal.java
@@ -36,6 +36,39 @@ public class Principal {
             System.out.println("Nombre: " + nombre);
         }
         
+        System.out.println("Métodos adicionales");
+        
+        int indice = nombres.indexOf("Levi");
+        
+        if(indice>=0)
+            System.out.println("Levi Existe, indice: " + indice);
+        else
+            System.out.println("Levi No Existe");
+        
+        boolean existe = nombres.contains("Zeke");        
+        if(existe)
+            System.out.println("Zeke Existe. ");
+        else
+            System.out.println("Zeke No Existe.");
+        
+        System.out.println("**** Eliminar elementos *****");
+        
+        boolean fueEliminado = nombres.remove("Luis");
+        if(fueEliminado)
+            System.out.println("Luis Eliminado.");
+        else
+            System.out.println("Luis No Eliminado.");
+        
+        String nombreEliminado = nombres.remove(2);
+        System.out.println("El nombre eliminado es: " + nombreEliminado);
+        
+        System.out.println("**** Limpiar colección ****");
+        System.out.println("Cantidad: " + nombres.size());
+        nombres.clear();
+        System.out.println("Cantidad: " + nombres.size());
+        
+        
+        
     }
     
 }
