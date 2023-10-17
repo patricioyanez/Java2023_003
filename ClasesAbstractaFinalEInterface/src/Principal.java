da4b8be73e3c6d12717a08104424024cc953615d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-17 10:23:22 -0300

    update

--- ClasesAbstractaFinalEInterface/src/Principal.java
+++ ClasesAbstractaFinalEInterface/src/Principal.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author patri
- */
 public class Principal {
-    
+    public static void main(String[] args) {
+        // Animal animal = new Animal();
+        String nombre = "";
+        
+    }
 }
