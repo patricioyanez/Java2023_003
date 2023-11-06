fbf48a79349cf86cc0d3fc530a37d25d50b7a975
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-06 18:21:42 -0300

    Update Conexion.java

--- EjemploInterfazGrafica003/src/BD/Conexion.java
+++ EjemploInterfazGrafica003/src/BD/Conexion.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author administrador
- */
+
 public class Conexion {
 
     public Connection obtenerConexion() {
