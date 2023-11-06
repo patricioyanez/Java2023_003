cb6b3c51f216dbf322ad23262a057107e2c3cdc2
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-06 18:20:49 -0300

    Update BD.sql

--- EjemploInterfazGrafica003/src/BD/BD.sql
+++ EjemploInterfazGrafica003/src/BD/BD.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE taller003;
+
 CREATE TABLE Marca
 (
     id          int             primary key auto_increment,
@@ -13,6 +15,6 @@ insert into marca values (null, 'Mazda', 1);
 insert into marca values (null, 'Chevrolet', 1);
 
 
-           -- Ejercicio 18: Crear Tabla, modelo y Formulario
-            -- para la entidad Cargo (gerente, vendedor, etc)
-            -- los atributos son: id, nombre y habilitado.
+-- Ejercicio 18/19: Crear Tabla, modelo, controlador y Formulario
+ -- para la entidad Cargo (gerente, vendedor, etc)
+ -- los atributos son: id, nombre y habilitado.
