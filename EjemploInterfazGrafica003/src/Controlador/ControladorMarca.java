dc113b27900cfceb2a60095dffb861b68cc0c6d3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-06 17:54:47 -0300

    update

--- EjemploInterfazGrafica003/src/Controlador/ControladorMarca.java
+++ EjemploInterfazGrafica003/src/Controlador/ControladorMarca.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
-/**
- *
- * @author patri
- */
+import Modelo.Marca;
+import bd.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ControladorMarca {
+    public boolean agregar(Marca marca)
+    {
+        try
+        {
+            Conexion con = new Conexion();
+            Connection cx = con.obtenerConexion();
+            String sql = "INSERT INTO MARCA (NOMBRE, HABILITADO) VALUES (?,?)";
+            PreparedStatement st;
+            st = cx.prepareStatement(sql);
+            st.setString(1, marca.getNombre());
+            st.setBoolean(2, marca.getHabilitado());
+            st.executeUpdate();
+            st.close();
+            cx.close();            
+            return true;
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return false;
+    }
+    public boolean actualizar(Marca marca)
+    {
+        return true;
+    }
+    public boolean eliminar(int id)
+    {
+        return true;
+    }
+    
+    public Marca buscarPorId(int id)
+    {
+        return null;
+    }
+    
+    public ArrayList<Marca> listarTodos()
+    {
+        return null;
+    }
     
 }
